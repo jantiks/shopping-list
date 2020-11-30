ceb0a9b8c89e08e678c6c1600feb5c37c926a6ba
jantiks <58886410+jantiks@users.noreply.github.com>
2020-11-30 14:35:50 +0400

    project is ready , it has refresh and add buttons

--- Shopping list/ViewController.swift
+++ Shopping list/ViewController.swift
@@ -8,12 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var shoppingList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addShopping))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(start))
+        
+        start()
     }
+    
+    @objc func start() {
+        shoppingList.removeAll()
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        
+        return cell
+    }
+    
+    @objc func addShopping() {
+        let ac = UIAlertController(title: "Add shopping list item ", message: "type on text field", preferredStyle: .alert)
+        ac.addTextField()
+    
+        let action = UIAlertAction(title: "add", style: .default) {
+            [weak self,weak ac] _ in
+            if let text = ac?.textFields?[0].text {
+                self?.shoppingList.insert(text, at: 0)
+                let indexPath = IndexPath(row: 0, section: 0)
+                self?.tableView.insertRows(at: [indexPath], with: .automatic)
+            }
+        }
+        
+        ac.addAction(action)
+        present(ac,animated: true)
+        
+    }
+    
+    
 
 
 }
